502b8aa1fdadc209671e3e4888c44f44772aba61
qmen <275169263@qq.com>
2019-06-13 17:42:53 +0800

    m

--- saturn-executor/src/run.sh
+++ saturn-executor/src/run.sh
@@ -3,4 +3,6 @@ sleep 11
 
 # start executor
 chmod +x /apphome/saturn-executor-3.3.1/bin/saturn-executor.sh
-/apphome/saturn-executor-3.3.1/bin/saturn-executor.sh start -n jobcenter.erp.wms -r foreground -jmx 8080
+/apphome/saturn-executor-3.3.1/bin/saturn-executor.sh start -n jobcenter.erp.wms  -jmx 8080
+
+sleep 100*100
